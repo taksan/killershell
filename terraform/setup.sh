3850b6d351cb176b58890fab5bf83df8f2f0dfd4
Takeuchi <g.takeuchi@gmail.com>
2024-07-06 01:10:43 -0300

    Improves setup

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,14 +1,18 @@
-echo "Downloading terraform"
-
+echo "Installing terraform"
 wget https://releases.hashicorp.com/terraform/1.9.1/terraform_1.9.1_linux_amd64.zip
-
-echo "unzipping"
 unzip terraform*zip
-
-echo "installing"
 mv terraform /usr/bin/
 
-echo "configuring aws credentials"
+
+echo "Installing awscli v2"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install -b /usr/bin
+
+rm *zip
+
+
+echo "Configuring aws credentials"
 
 mkdir -p .aws
 chmod 700 .aws
@@ -16,8 +20,16 @@ cat <<EOF > .aws/credentials
 [default]
 aws_access_key_id = test
 aws_secret_access_key = test
-region = sa-east-1
 EOF
+
+cat <<EOF > .aws/config
+[profile default]
+region=us-east-1
+output=json
+endpoint_url=http://localhost:4566
+EOF
+
+chmod -R 600 .aws
 
 echo done
 
