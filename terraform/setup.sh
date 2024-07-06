827f436eea4a859717248c01faa8d9b44b482201
Takeuchi <g.takeuchi@gmail.com>
2024-07-06 00:25:05 -0300

    install terraform without confirmation

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -2,7 +2,7 @@ apt install wget curl -y
 
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
+sudo apt update && sudo apt install -y terraform
 
 mkdir -p .aws
 chmod 700 .aws
