95848b4ab09e2284c56fbf733c1b5727825d8ecd
Takeuchi <g.takeuchi@gmail.com>
2024-07-06 00:21:21 -0300

    moves localstack to foreground

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -4,13 +4,6 @@ wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sha
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
 
-docker run \
-  --rm -d \
-  -p 127.0.0.1:4566:4566 \
-  -p 127.0.0.1:4510-4559:4510-4559 \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  localstack/localstack
-
 mkdir -p .aws
 chmod 700 .aws
 cat <<EOF > .aws/credentials
--- terraform/wait.sh
+++ terraform/wait.sh
@@ -1,3 +1,12 @@
 echo Aguardando a preparação do ambiente
 while [ ! -f /tmp/background0 ]; do sleep 1; done
+
+echo "Iniciando o localstack"
+docker run \
+  --rm -d \
+  -p 127.0.0.1:4566:4566 \
+  -p 127.0.0.1:4510-4559:4510-4559 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  localstack/localstack
+
 echo Preparação concluída
