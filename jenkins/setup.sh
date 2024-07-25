61f357071c34afeaa06cc1108ce0eca70da2d446
Takeuchi <g.takeuchi@gmail.com>
2024-07-25 13:32:31 -0300

    fix: disables CSRF protection because jenkins doesn't work well behind killercoda's proxy

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,5 +1,5 @@
-docker run --name jenkins --rm -d -p 443:8080  \
-       --env JAVA_OPTS="-Dhudson.Main.development=true -Djenkins.install.runSetupWizard=false" \
+docker run --name jenkins --rm -d -p 8080:8080  \
+       --env JAVA_OPTS="-Dhudson.Main.development=true -Djenkins.install.runSetupWizard=false -Dhudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION=true" \
        jenkins/jenkins:alpine3.20-jdk21
 
 touch /tmp/jenkins-up
