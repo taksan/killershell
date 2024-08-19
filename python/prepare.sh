438826a40519e30f5624c395fa41814a3ca952ba
Takeuchi <g.takeuchi@gmail.com>
2024-08-19 11:38:35 -0300

    fix: try to fix venv

--- python/prepare.sh
+++ python/prepare.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-apt install -y python3.8-venv
+echo "Setup running! Please wait up to 120 seconds."
+
+waitTries=0
+until [ -f /tmp/setup-complete ]; do
+  sleep 1
+  waitTries=$(( waitTries+1 ))
+  if [ "$waitTries" -gt 120 ]; then
+    echo "Failed to detect setup complete!" >&2
+  fi
+done
+
+echo "Setup complete successfully!"
 
 python3 -m venv venv
 source venv/bin/activate
---
+++ python/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+apt install -y python3.8-venv
+
+touch /tmp/setup-complete
