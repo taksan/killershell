a8770557ee6964a8a6e04d25635b50038b6c5616
Takeuchi <g.takeuchi@gmail.com>
2025-11-05 22:20:00 -0300

    feat: upgrade Jenkins version

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,5 +1,7 @@
 docker run --name jenkins --rm -d -p 8080:8080  \
-       --env JAVA_OPTS="-Dhudson.Main.development=true -Djenkins.install.runSetupWizard=false -Dhudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION=true" \
-       jenkins/jenkins:alpine3.20-jdk21
+       --env JAVA_OPTS="-Dhudson.Main.development=true \
+       -Djenkins.install.runSetupWizard=false \
+       -Dhudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION=true" \
+       jenkins/jenkins:alpine3.22-jdk21
 
 touch /tmp/jenkins-up
