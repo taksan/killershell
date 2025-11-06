c3016b4bab8fdfddc2675dc86aaba8548eab9aec
Takeuchi <g.takeuchi@gmail.com>
2025-11-05 22:59:47 -0300

    feat: replaces by a custom jenkins image with workflow plugins

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -2,6 +2,6 @@ docker run --name jenkins --rm -d -p 8080:8080  \
        --env JAVA_OPTS="-Dhudson.Main.development=true \
        -Djenkins.install.runSetupWizard=false \
        -Dhudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION=true" \
-       jenkins/jenkins:alpine3.22-jdk21
+       taksan/jenkins:v1.0.0
 
 touch /tmp/jenkins-up
